6fb4ceb334aa78e23653b68b2e71e71293ab376f
hariinfo <hari.info@gmail.com>
2024-09-02 17:41:30 -0500

    updated embed model routine

--- api/embed.py
+++ api/embed.py
@@ -1,49 +1,19 @@
-import os
-from datetime import datetime
-from werkzeug.utils import secure_filename
-from langchain_community.document_loaders import UnstructuredPDFLoader, WebBaseLoader, JSONLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from get_vector_db import get_vector_db
+from llama_index.core import Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.embeddings.nomic import NomicEmbedding
+from llama_index.embeddings.ollama import OllamaEmbedding
 
-TEMP_FOLDER = os.getenv('TEMP_FOLDER', './_temp')
+    
+def embed_model():
+    #Using embedding model hosted by Ollama
+    embed_model = OllamaEmbedding(
+        model_name="nomic-embed-text",
+        base_url="http://localhost:11434",
+        ollama_additional_kwargs={"mirostat": 0},
+    )
 
-# Function to check if the uploaded file is allowed (only PDF files)
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'json'}
+    # using embedding model from hugging face
+    #embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+    #embed_model = NomicEmbedding(model_name="nomic-embed-text")
 
-# Function to save the uploaded file to the temporary folder
-def save_file(file):
-    # Save the uploaded file with a secure filename and return the file path
-    ct = datetime.now()
-    ts = ct.timestamp()
-    filename = str(ts) + "_" + secure_filename(file.filename)
-    file_path = os.path.join(TEMP_FOLDER, filename)
-    file.save(file_path)
-
-    return file_path
-
-# Function to load and split the data from the PDF file
-def load_and_split_data(file_path):
-    # Load the json file and split the data into chunks
-    print("loading data......")
-    loader = JSONLoader(file_path=file_path, jq_schema='.messages[]', text_content=False)
-    data = loader.load()
-    print(f"Loaded {data} documents from {file_path}")
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=7500, chunk_overlap=100)
-    chunks = text_splitter.split_documents(data)
-    return chunks
-
-# Main function to handle the embedding process
-def embed(file):
-    # Check if the file is valid, save it, load and split the data, add to the database, and remove the temporary file
-    if file.filename != '' and file and allowed_file(file.filename):
-        file_path = save_file(file)
-        chunks = load_and_split_data(file_path)
-        db = get_vector_db()
-        db.add_documents(chunks)
-        db.persist()
-        os.remove(file_path)
-
-        return True
-
-    return False
+    return  embed_model
